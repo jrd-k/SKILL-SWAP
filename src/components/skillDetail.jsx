431da0ace43e49ba1403cbaac8fdc73097053e57
jared kiprop <jaredassociates@gmail.com>
2025-07-23 00:18:21 +0300

    SkillDetail.jsx editing

---
+++ src/components/skillDetail.jsx
@@ -0,0 +1,27 @@
+// src/components/SkillDetail.jsx
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function SkillDetail() {
+  const { id } = useParams();
+  const [skill, setSkill] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/skills/${id}`)
+      .then((res) => res.json())
+      .then((data) => setSkill(data));
+  }, [id]);
+
+  if (!skill) return <p>Loading skill info...</p>;
+
+  return (
+    <div>
+      <h2>{skill.title}</h2>
+      <p>{skill.description}</p>
+      <p><strong>Location:</strong> {skill.location}</p>
+      <p><strong>Level:</strong> {skill.level}</p>
+    </div>
+  );
+}
+
+export default SkillDetail;
