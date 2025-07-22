be1acdde4f2c3a82abf978f7074538f77cc5e468
jared kiprop <jaredassociates@gmail.com>
2025-07-22 19:45:03 +0300

    adding tags to the app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SkillList from "./components/SkillList";
+import SkillDetail from "./components/SkillDetail";
+import AddSkillForm from "./components/AddSkillForm";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<SkillList />} />
+        <Route path="/skills/:id" element={<SkillDetail />} />
+        <Route path="/add-skill" element={<AddSkillForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
