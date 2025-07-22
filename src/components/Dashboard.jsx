6ffa1fc3c0ef5c29feea07c51bffb7b535b9e192
jared kiprop <jaredassociates@gmail.com>
2025-07-23 00:40:31 +0300

    Dashboard.jsx

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+// src/components/Dashboard.jsx
+function Dashboard() {
+  return (
+    <div>
+      <h2>Your Dashboard</h2>
+      <p>This page will display your posted and favorited skills.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
