3be0c087655dbf83eb34a64dbdf9ed60779e5630
jared kiprop <jaredassociates@gmail.com>
2025-07-22 22:41:22 +0300

    adding home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to SkillSwap!</h1>
+      <p>Connect, share, and request skills in your local community.</p>
+    </div>
+  );
+}
+
+export default Home;
